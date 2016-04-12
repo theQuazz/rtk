e561de357c9e18b76451cdd8dc971fe8e695ede5
Jesse Rogers <djrquazz@gmail.com>
2016-04-12 13:06:44 -0400

    Add debugging to SRR & fix put_tid

--- kernel/communication.c
+++ kernel/communication.c
@@ -3,6 +3,7 @@
 #include "../include/common.h"
 #include "../include/communication.h"
 #include "../lib/string.h"
+#include "../lib/debug.h"
 
 static struct msg_queue_node nodes[TASK_MAX][TASK_MAX];
 static struct task_queue msg_queues[TASK_MAX];
@@ -15,11 +16,15 @@ void InitCommunication( void ) {
 }
 
 int SendProxy( int tid, void *msg, int msglen, void *reply, int replylen ) {
+  Debugln( "Sending message (%d) to %d", msg, tid );
+
   if ( ! IsValidTid( tid ) ) {
+    Debugln( "Invalid tid, aborting!" );
     return ERR_SEND_IMPOSSIBLE_TID;
   }
 
   if ( ! IsTaskAlive( tid ) ) {
+    Debugln( "Non-existent tid, aborting!" );
     return ERR_SEND_NONEXISTENT_TASK;
   }
 
@@ -44,26 +49,32 @@ int SendProxy( int tid, void *msg, int msglen, void *reply, int replylen ) {
   SetCurrentTaskState( SEND_BLOCKED );
 
   if ( GetTaskState( tid ) == RECEIVE_BLOCKED ) {
+    Debugln( "Task %d awaiting message, resuming", tid );
+
     struct msg_queue_node *receiver_node = &nodes[tid][tid];
 
     node->put_tid = receiver_node->put_tid;
     node->receive_buffer = receiver_node->receive_buffer;
     node->receive_buffer_size = receiver_node->receive_buffer_size;
 
-    return CompleteReceive( mytid );
+    return CompleteReceive( tid );
   }
 
   return RETURN_STATUS_OK;
 }
 
 int ReceiveProxy( int *tid, void *msg, int msglen ) {
+  Debugln( "Receiving message" );
+
   const int mytid = GetCurrentTid();
 
   if ( ! msg_queues[mytid].first ) {
+    Debugln( "No messages in queue, waiting" );
+
     SetCurrentTaskState( RECEIVE_BLOCKED );
 
     struct msg_queue_node n;
-    n.to_tid = mytid;
+    n.put_tid = tid;
     n.receive_buffer = msg;
     n.receive_buffer_size = msglen;
 
@@ -73,6 +84,8 @@ int ReceiveProxy( int *tid, void *msg, int msglen ) {
   }
 
   struct msg_queue_node *n = ( struct msg_queue_node* )( msg_queues[mytid].first );
+
+  Debugln( "Message available, retrieving (%d)", n->msg_buffer );
 
   n->put_tid = tid;
   n->receive_buffer = msg;
@@ -87,26 +100,33 @@ int CompleteReceive( int tid ) {
   memcpy( n->receive_buffer, n->msg_buffer, n->receive_buffer_size );
   *( n->put_tid ) = n->from_tid;
 
-  SetTaskState( n->to_tid, READY );
   SetTaskState( n->from_tid, REPLY_BLOCKED );
+  SetTaskState( n->to_tid, READY );
   SetTaskReturnValue( n->to_tid, n->msg_buffer_size );
+
+  Debugln( "Message (%d) sent from task %d to task %d", n->msg_buffer, n->from_tid, n->to_tid );
 
   return n->msg_buffer_size;
 }
 
 int ReplyProxy( int tid, void *reply, int replylen ) {
+  Debugln( "Replying to task %d", tid );
+
   const int mytid = GetCurrentTid();
   struct msg_queue_node *n = &nodes[mytid][tid];
 
   if ( ! IsValidTid( tid ) ) {
+    Debugln( "Invalid tid, aborting!" );
     return ERR_SEND_IMPOSSIBLE_TID;
   }
 
   if ( ! IsTaskAlive( tid ) ) {
+    Debugln( "Non-existent tid, aborting!" );
     return ERR_SEND_NONEXISTENT_TASK;
   }
 
   if ( GetTaskState( tid ) != REPLY_BLOCKED ) {
+    Debugln( "Task is not reply blocked, aborting!" );
     return ERR_NOT_REPLY_BLOCKED;
   }
 
@@ -116,6 +136,7 @@ int ReplyProxy( int tid, void *reply, int replylen ) {
   SetTaskReturnValue( n->from_tid, replylen );
 
   if ( replylen > n->reply_buffer_size ) {
+    Debugln( "Reply buffer is too small!" );
     return ERR_BUFFER_TOO_SMALL;
   }
 
