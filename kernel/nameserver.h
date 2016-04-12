b179a539883517ef9b879d45654988493077567b
Jesse Rogers <djrquazz@gmail.com>
2016-04-12 14:08:02 -0400

    NameServer tweaks

--- kernel/nameserver.h
+++
@@ -1,8 +0,0 @@
-#ifndef __K_NAMESERVER_H__
-#define __K_NAMESERVER_H__
-
-int RegisterAsProxy( char *name );
-
-int WhoIsProxy( char *name );
-
-#endif
--- usr/nameserver.c
+++ usr/nameserver.c
@@ -1,11 +1,65 @@
 #include "nameserver.h"
+#include "../include/common.h"
+#include "../include/task.h"
 #include "../lib/hash.h"
 #include "../lib/hashtable.h"
+#include "../lib/string.h"
+#include "../lib/debug.h"
+
+static int nameserver_tid = -1;
+
+int RegisterAs( char *name ) {
+  struct NameServerRegisterAsResponse response;
+  struct NameServerRequest request;
+
+  request.type = NS_REGISTER_AS;
+
+  const int name_length = strnlen( name, NS_NAME_BUFFER_SIZE - 1 );
+  strncpy( request.name, name, name_length );
+
+  const int err = Send( nameserver_tid, &request, sizeof( request ), &response, sizeof( response ) );
+
+  switch ( err ) {
+    case RETURN_STATUS_OK: {
+      switch ( response.status ) {
+        case RETURN_STATUS_OK: return RETURN_STATUS_OK;
+        default: return ERR_LOOKUP_FAILED;
+      }
+    };
+    default: return ERR_INVALID_NAMESERVER_TID;
+  }
+}
+
+int WhoIs( char *name ) {
+  struct NameServerWhoIsResponse response;
+  struct NameServerRequest request;
+
+  request.type = NS_WHO_IS;
+
+  const int name_length = strnlen( name, NS_NAME_BUFFER_SIZE - 1 );
+  strncpy( request.name, name, name_length );
+
+  const int err = Send( nameserver_tid, &request, sizeof( request ), &response, sizeof( response ) );
+
+  switch ( err ) {
+    case RETURN_STATUS_OK: {
+      switch ( response.status ) {
+        case RETURN_STATUS_OK: return response.whois_tid;
+        default: return ERR_LOOKUP_FAILED;
+      }
+    };
+    default: return ERR_INVALID_NAMESERVER_TID;
+  }
+}
 
 void NameServer() {
+  Debugln( "NameServer starting up..." );
+
   struct HashTable ht;
 
   HashTableInitialize( &ht );
+
+  nameserver_tid = MyTid();
 
   for ( ;; ) {
     int tid = -1;
