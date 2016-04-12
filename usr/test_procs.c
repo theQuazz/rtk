a629516742a6abbd51f2f0be399433b3c1052a21
Jesse Rogers <djrquazz@gmail.com>
2016-04-12 13:53:58 -0400

    More testing (return codes this time)

--- usr/test_procs.c
+++ usr/test_procs.c
@@ -5,40 +5,24 @@
 #include "../lib/util.h"
 
 void usr1() {
-  char msg[] = "Hi from usr1!";
-  char reply[20];
+  char msg[] = "Test message usr1";
+  char buf[20];
 
-  Print( 0, "usr1: starting\n" );
-
-  for ( int i = 0; ; i += 4 ) {
-    const int reply_size = Send( 3, msg, sizeof( msg ), reply, 20 );
-
-    Print( 0, "usr1: got back \"%s\" (%d)\n", reply, reply_size );
-
-    DelaySeconds( 1 );
-    Pass();
-  }
+  Print( 0, "usr1: Send( -1, %d, %d, %d, %d ) => err( %d )\n", msg, sizeof( msg ), buf, sizeof( buf ), Send( -1, msg, sizeof( msg ), buf, sizeof( buf ) ) );
+  Print( 0, "usr1: Send( TASK_MAX, %d, %d, %d, %d ) => err( %d )\n", msg, sizeof( msg ), buf, sizeof( buf ), Send( 1024, msg, sizeof( msg ), buf, sizeof( buf ) ) );
+  Print( 0, "usr1: Send( Zombied, %d, %d, %d, %d ) => err( %d )\n", msg, sizeof( msg ), buf, sizeof( buf ), Send( 1, msg, sizeof( msg ), buf, sizeof( buf ) ) );
+  Print( 0, "usr1: Send( NotStarted, %d, %d, %d, %d ) => err( %d )\n", msg, sizeof( msg ), buf, sizeof( buf ), Send( 6, msg, sizeof( msg ), buf, sizeof( buf ) ) );
 }
 
 void usr2() {
-  char msg[20];
-  int fromtid;
+  char msg[] = "Test message usr1 MORE STUFF MORE STUFF MORE STUFF";
 
-  Print( 0, "usr2: starting\n" );
-
-  for ( int i = 1; ; i += 4 ) {
-    const int msg_size = Receive( &fromtid, msg, sizeof( msg ) );
-
-    Print( 0, "usr2: %d said \"%s\" (%d)\n", fromtid, msg, msg_size );
-
-    char reply[] = "Thanks!";
-    const int err = Reply( fromtid, reply, sizeof( reply ) );
-
-    Print( 0, "usr2: err( %d )\n", err );
-
-    DelaySeconds( 1 );
-    Pass();
-  }
+  Print( 0, "usr2: Reply( -1, %d, %d ) => err( %d )\n", msg, sizeof( msg ), Reply( -1, msg, sizeof( msg ) ) );
+  Print( 0, "usr2: Reply( TASK_MAX, %d, %d ) => err( %d )\n", msg, sizeof( msg ), Reply( 1024, msg, sizeof( msg ) ) );
+  Print( 0, "usr2: Reply( Zombied, %d, %d ) => err( %d )\n", msg, sizeof( msg ), Reply( 1, msg, sizeof( msg ) ) );
+  Print( 0, "usr2: Reply( NotStarted, %d, %d ) => err( %d )\n", msg, sizeof( msg ), Reply( 6, msg, sizeof( msg ) ) );
+  Print( 0, "usr2: Reply( TooSmallBuffer, %d, %d ) => err( %d )\n", msg, sizeof( msg ), Reply( 5, msg, sizeof( msg ) ) );
+  Print( 0, "usr2: Reply( NotReplyBlocked, %d, %d ) => err( %d )\n", msg, sizeof( msg ), Reply( 4, msg, sizeof( msg ) ) );
 }
 
 void usr3() {
@@ -57,7 +41,7 @@ void usr3() {
 
     Print( 0, "usr3: err( %d )\n", err );
 
-    DelaySeconds( 1 );
+    BusyDelayMs( 100 );
     Pass();
   }
 }
@@ -73,7 +57,7 @@ void usr4() {
 
     Print( 0, "usr4: got back \"%s\" (%d)\n", reply, reply_size );
 
-    DelaySeconds( 1 );
+    BusyDelayMs( 100 );
     Pass();
   }
 }
