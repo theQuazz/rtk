f9790b2648a28bc12916e4550c29be6afe97e041
Jesse Rogers <djrquazz@gmail.com>
2016-04-15 11:19:18 -0400

    Math utils

---
+++ lib/math.c
@@ -0,0 +1,9 @@
+#include "math.h"
+
+int min( int a, int b ) {
+  return a < b ? a : b;
+}
+
+int max( int a, int b ) {
+  return a > b ? a : b;
+}
---
+++ lib/math.h
@@ -0,0 +1,7 @@
+#ifndef __MATH_H__
+#define __MATH_H__
+
+int min( int a, int b );
+int max( int a, int b );
+
+#endif
