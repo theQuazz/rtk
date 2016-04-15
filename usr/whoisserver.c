9745a552bf9eb0e56a239b495881bf32acc438c4
Jesse Rogers <djrquazz@gmail.com>
2016-04-15 11:20:48 -0400

    Add whois server

---
+++ usr/whoisserver.c
@@ -0,0 +1,31 @@
+#include "whoisserver.h"
+#include "../include/nameserver.h"
+#include "../include/communication.h"
+#include "../lib/string.h"
+#include "../lib/print.h"
+
+#include <stddef.h>
+
+void WhoIsServer( void ) {
+  while ( RegisterAs( "whois" ) == ERR_INVALID_NAMESERVER_TID );
+
+  for ( ;; ) {
+    char buf[128];
+    int sender_tid;
+
+    Receive( &sender_tid, buf, sizeof( buf ) );
+
+    strtok( buf, " " );
+    int tid = WhoIs( strtok( NULL, " " ) );
+    int ret = 0;
+
+    if ( tid >= 0 ) {
+      Print( 0, "%d\n", tid );
+    }
+    else {
+      ret = tid;
+    }
+
+    Reply( sender_tid, &ret, sizeof( ret ) );
+  }
+}
---
+++ usr/whoisserver.h
@@ -0,0 +1,6 @@
+#ifndef __WHOISSERVER_H_
+#define __WHOISSERVER_H_
+
+void WhoIsServer( void );
+
+#endif
